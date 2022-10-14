d8298263ce5c6bd33c7108c56975936e45b9ccd6
jgbijlsma <jbijlsma.p@gmail.com>
2022-10-14 09:12:34 +0200

    initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/items")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
+  function renderItems() {
+    return items.map((item, i) => {
+      return (
+        <div key={i}>
+          <h3>{item.name}</h3>
+          <p>Price: {item.price}</p>
+        </div>
+      );
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Example webshop</h1>
+      {renderItems()}
+    </main>
   );
 }
 
